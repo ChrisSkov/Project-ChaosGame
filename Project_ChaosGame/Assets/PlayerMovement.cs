bd7ecbd4e22a506949700c1527d25ec23b971689
Chris Skov <stofferdyr@gmail.com>
2021-02-16 08:37:50 +0100

    refactor movement code

--- Project_ChaosGame/Assets/PlayerMovement.cs
+++ Project_ChaosGame/Assets/PlayerMovement.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
-    [SerializeField] Animator playerAnim;
     public float movementSpeed = 5f;
     public float turnSpeed = 5f;
-    Playermap controls;
-    public Vector2 move;
-    Vector2 rotate;
+
     Vector3 movementDirection;
     Rigidbody rb;
-    private void Awake()
-    {
-        controls = new Playermap();
 
-        controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => move = Vector2.zero;
-        controls.Player.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
-        controls.Player.Rotate.canceled += ctx => rotate = Vector2.zero;
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -35,10 +24,17 @@ public class PlayerMovement : MonoBehaviour
         MoveThePlayer();
         TurnThePlayer();
     }
+    public void SetupBehaviour()
+    {
+        SetGameplayCamera();
+    }
 
+    void SetGameplayCamera()
+    {
+        mainCamera = Camera.main;
+    }
     void TurnThePlayer()
     {
-        movementDirection = new Vector3(move.x, 0, move.y);
         if (movementDirection.sqrMagnitude > 0.01f)
         {
 
@@ -51,37 +47,17 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    public void UpdateMovementData(Vector3 newMovementDirection)
+    {
+        movementDirection = newMovementDirection;
+    }
 
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-    }
-    private void OnDisable()
-    {
-        controls.Player.Disable();
-    }
     void MoveThePlayer()
     {
-        movementDirection = new Vector3(move.x, 0, move.y);
-    
-    
-        playerAnim.SetFloat("forwardSpeed", movementDirection.magnitude);
-        // playerAnim.SetFloat("horizontalSpeed", MertInput.horizontal);
-        // playerAnim.SetFloat("forwardSpeed", MertInput.vertical);
-
-        //Equalize strictly veritcal/horizontal speed with diagonal speed
-        if (movementDirection.magnitude >= .05)
-        {
-            movementDirection = movementDirection.normalized;
-            playerAnim.SetBool("isRunning", true);
-        }
-        else
-        {
-            playerAnim.SetBool("isRunning", false);
-        }
         Vector3 movement = CameraDirection(movementDirection) * movementSpeed * Time.deltaTime;
         rb.MovePosition(transform.position + movement);
     }
+
     Vector3 CameraDirection(Vector3 movementDirection)
     {
         var cameraForward = mainCamera.transform.forward;
